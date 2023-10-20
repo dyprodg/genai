fe094ba2616bff54712a960a95193653ce75a696
dyprodg <dennisdiepolder@outlook.com>
2023-10-20 17:21:38 +0200

    sidebar setup

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,11 @@
-import { UserButton } from "@clerk/nextjs";
+
 
 
 const DashboardPage = () => {
     return(
         <div>
             <p>Dashboard Page (Protected)</p>
-            <UserButton afterSignOutUrl="/" />
+            
         </div>
     )
 }
