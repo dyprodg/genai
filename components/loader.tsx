36efa2d48ef4eafbe6419ceef8c0e0ad11e9d9d2
dyprodg <dennisdiepolder@outlook.com>
2023-10-21 16:09:15 +0200

    loaderfinish

--- components/loader.tsx
+++ components/loader.tsx
@@ -3,15 +3,16 @@ import Image from "next/image"
 export const Loader = () => {
     return (
         <div className="h-full flex flex-col gap-y-4 items-center justify-center">
-            <div className="w-10 h-10 relative animate-spin">
+            <div className="w-14 h-14 relative animate-spin border rounded-full">
                 <Image 
+                    className="bg-gray-300 rounded-full p-1"
                     alt="logo"
                     layout="fill"
                     src='/logo.png'
                 />
             </div>
             <p className="text-sm text-muted-foreground">
-                AIOrchard is dropping some fruites
+                AIOrchard is dropping some fruites...
             </p>
         </div>
     )
