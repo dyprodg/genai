1423fff21ccf272e3e356aa32b3c7356c3ecc9f7
dyprodg <dennisdiepolder@outlook.com>
2023-11-06 15:42:12 +0100

    Video Generation Working

---
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,119 @@
+'use client'
+
+import axios from 'axios';
+import * as z from 'zod';
+import { Heading } from "@/components/heading";
+import { VideoIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { formSchema } from './constants';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Empty } from '@/components/empty';
+import { Loader } from '@/components/loader';
+import { cn } from '@/lib/utils';
+
+
+
+const VideoGenerationPage = () => {
+
+    const router = useRouter();
+    
+    const [video, setVideo] = useState<string>();
+
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            prompt: ''
+        }
+    })
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            setVideo(undefined)
+            
+            const response = await axios.post('/api/video', values)
+
+            setVideo(response.data[0]);
+            form.reset();
+        } catch (error: any) {
+            //Todo: open pro model
+            console.log(error)
+        } finally {
+            router.refresh();
+        }
+    }
+
+    return (
+        <div>
+            
+            <div className="p-4 lg:px-8">
+                {/* Form Comp */}
+                <div>
+                <Heading 
+                title='Video Generation'
+                description='Turn your words into a Video'
+                icon={VideoIcon}
+                iconColor='text-emerald-500'
+                bgColor='bg-violet-500/10'
+            />
+                    <Form {...form}>
+                        <form 
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className='rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-md grid grid-cols-12 gap-2'
+                        >
+                            <FormField name='prompt'
+                            render={({field}) => (
+                                <FormItem className='col-span-12 lg:col-span-10'>
+                                    <FormControl className='m-0 p-0'>
+                                        <Input 
+                                            className='border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent'
+                                            disabled={isLoading}
+                                            placeholder='Type what you want to create...'
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}/>
+                            <Button 
+                            className='col-span-12 lg:col-span-2 w-full'
+                            disabled={isLoading}>
+                                Generate
+                            </Button>
+                        </form>
+                    </Form>
+                </div>
+
+                {/* Chat comp */}
+                <div className='mt-8 space-y-4 w-full'>
+                    {isLoading && (
+                        <div className='p-8 rounded-lg w-full flex items-center justify-center bg-muted'>
+                            <Loader />
+                        </div>
+                    )}
+                    {!video && !isLoading && (
+                        <Empty 
+                            label='No video generated yet'
+                            imagesource='/video.png'
+                        />
+                    )}
+                </div>
+                {video && (
+                    <video className='w-full aspect-video mt-8 rounded-lg border bg-black'
+                    controls>
+                        <source src={video}/>
+                    </video>
+
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default VideoGenerationPage;
---
+++ app/api/video/route.ts
@@ -0,0 +1,52 @@
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+import Replicate from 'replicate';
+
+
+const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_TOKEN
+})
+
+export async function POST(req: Request) {
+    
+    
+
+    try {
+        const { userId } = auth();
+        const body = await req.json();
+        const { prompt } = body;
+
+        if(!userId) {
+            return new NextResponse('Unauthorized', { status: 401});
+        }
+
+        if(!prompt) {
+            return new NextResponse('Prompt is required', {status: 400});
+        }
+
+        const response = await replicate.run(
+            "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+            {
+              input: {
+                fps: 8,
+                model: "xl",
+                width: 1024,
+                height: 576,
+                prompt: prompt,
+                batch_size: 1,
+                num_frames: 24,
+                init_weight: 0.5,
+                guidance_scale: 17.5,
+                negative_prompt: "dust, noisy, washed out, ugly, distorted, broken",
+                
+                num_inference_steps: 50
+              }
+            }
+          );
+
+        return NextResponse.json(response);
+    } catch (error) {
+       console.log("[VIDEOGENERATION_ERROR]", error);
+       return new NextResponse("Internal Error", {status: 500});
+    }
+}
