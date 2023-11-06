6dac102f2a3301e355269857d8ef816fbb32a084
dyprodg <dennisdiepolder@outlook.com>
2023-11-06 16:28:11 +0100

    prismainit

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -2,13 +2,7 @@ import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 import Replicate from 'replicate';
 
-interface Message {
-    content: string;
-}
 
-interface RequestBody {
-    messages: Message[];
-}
 
 const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN
