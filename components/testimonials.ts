8ae61a8551189f54338384e61675fc0557641c87
dyprodg <dennisdiepolder@outlook.com>
2023-11-10 18:43:24 +0100

    betarelease

---
+++ components/testimonials.ts
@@ -0,0 +1,26 @@
+export const testimonials = [
+    {
+      name: "Joel",
+      avatar: "J",
+      title: "Software Engineer",
+      description: "This is the best application I've ever used!",
+    },
+    {
+      name: "John",
+      avatar: "J",
+      title: "Designer",
+      description: "I use this daily for generating new photos!",
+    },
+    {
+      name: "Mark",
+      avatar: "M",
+      title: "CEO",
+      description: "This app has changed my life, cannot imagine working without it!",
+    },
+    {
+      name: "Mary",
+      avatar: "M",
+      title: "CFO",
+      description: "The best in class, definitely worth the premium subscription!",
+    },
+  ];
