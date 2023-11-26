8b4460cf848a1399dfcfe2a32fc0c474f9da4120
dyprodg <dennisdiepolder@outlook.com>
2023-11-26 13:08:05 +0100

    redirect

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,17 @@
 import { LandingContent } from "@/components/landing-content";
 import { LandingHero } from "@/components/landing-hero";
 import { LandingNavbar } from "@/components/landing-navbar";
+import { auth } from '@clerk/nextjs';
+import { redirect } from "next/navigation";
 
 
 const LandingPage = () => {
+
+    const { userId } = auth();
+    if(userId) {
+        redirect('/dashboard')
+    }
+
     return(
         <div className="h-full">
             <LandingNavbar />
